8f9116086ba65eeafcdc48e5cf51a86043a57b78
jrankindev <jxrankin@gmail.com>
2021-01-15 13:13:06 -0500

    start

---
+++ main.ps1
@@ -0,0 +1,26 @@
+﻿#i want to see if i can open an existing excel document and make changes to it with powershell without the ImportExcel module
+
+#clear screen
+Clear-Host
+
+$path = "D:\Dropbox\DEV\excel\Powershell-Excel-Test\test.xlsx"
+
+$excel = New-Object -ComObject Excel.Application
+$workbook = $excel.Workbooks.Open($path)
+$page = "Sheet1"
+$ws = $workbook.worksheets | Where-Object {$_.Name -eq $page}
+
+$cells = $ws.Cells
+
+$row = 20
+$col = 7
+
+$cells.item($row,$col) = "test"
+
+$cells.item($row,$col)
+
+$workbook.Close($true)
+$excel.quit()
+
+$killID = ((get-process excel | Select-Object MainWindowTitle, ID, StartTime | Sort-Object StartTime)[-1]).Id
+Stop-Process -Id $killID
